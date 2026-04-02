91f8bb08150b9ffb12a497ad540f3c504307dc07
Antonio Astorino <antonio.astorino.phd@gmail.com>
2026-04-02 16:44:58 +0200

    Use sys_clk as clock and s1 to reset

--- 04-sipo/04-sipo-top.sv
+++ 04-sipo/04-sipo-top.sv
@@ -6,15 +6,30 @@ module top
         output reg [5:0] leds
     );
     parameter BITS = 6;
-    wire tied_on = 1'b1;
+    parameter FREQ = 4; // Frequency in Hz
     wire [BITS-1:0] sipo_val;
+
+    reg [24:0] count;
+    reg slow_clk;
+
+    always @(posedge sys_clk) begin
+        count <= count + 1;
+        if (count == 27000000 / FREQ) begin
+            count <= 0;
+        end
+        if (count < (27000000 / 2 / FREQ)) begin
+            slow_clk <= 1;
+        end else begin
+            slow_clk <= 0;
+        end
+    end
 
     sipoNbits
     #(.BITS(BITS))
     sipo_inst
     (
-        .clk(btn_s1),
-        .rst_n(tied_on),
+        .clk(slow_clk),
+        .rst_n(btn_s1),
         .s_in(btn_s2),
         .p_out(sipo_val)
     );
--- 04-sipo/sipo.sv
+++ 04-sipo/sipo.sv
@@ -5,9 +5,9 @@ module sipoNbits #(parameter BITS = 16)
         input wire clk,
         output reg [BITS-1:0] p_out
     );
-    always @(posedge clk) begin
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            p_out <= 'b0;
+            p_out <= ~'d0;
         end else begin
             p_out <= {p_out[BITS-2:0], s_in};
         end
